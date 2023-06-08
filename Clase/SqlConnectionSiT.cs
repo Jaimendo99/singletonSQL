1af6043020600c5ad3bc35400880f9161239ed9e
jaime <105898917+Jaimendo99@users.noreply.github.com>
2023-06-08 10:11:05 -0500

    Init Repo

---
+++ Clase/SqlConnectionSiT.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Clase
+{
+	public class SqlConnectionST
+	{
+		private readonly SqlConnection conn;
+
+		private SqlConnection()
+		{
+		}
+		
+
+		public static SqlConnectionST getInstance()
+		{
+			if (conn == null)
+			{
+				conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+			}
+			return conn;
+		}
+
+	}
+}
